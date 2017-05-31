2b8e507014b50820854c93a171ba944b60929fda
ericheep <ericheep@gmail.com>
2017-05-31 11:54:05 -0700

    getting this all cleaned up

--- miscellany/wsgi.py
+++ miscellany/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crystalwarp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "miscellany.settings")
 
 application = get_wsgi_application()
