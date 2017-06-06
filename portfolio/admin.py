b100d348c8becde7cd70644b436e99daf196a4fe
ericheep <ericheep@gmail.com>
2017-06-05 17:24:15 -0700

    working on schema

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Work, Performance
+from .models import Work, Performance, Venue, Image
 
 admin.site.register(Work)
 admin.site.register(Performance)
+admin.site.register(Venue)
+admin.site.register(Image)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 
 class Work(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     discipline = models.CharField(max_length=50)
-    created_date = models.DateTimeField()
+    created_date = models.DateField()
+    tags = ArrayField(
+        models.CharField(max_length=20, null=True),
+        null=True
+    )
 
     def add(self):
         self.save()
@@ -14,10 +19,21 @@ class Work(models.Model):
         return self.title
 
 
+class Venue(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+
+    def add(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+
 class Performance(models.Model):
     work = models.ForeignKey(Work)
+    venue = models.ForeignKey(Venue)
     date = models.DateTimeField()
-    venue = models.CharField(max_length=50)
     location = models.CharField(max_length=50)
 
     def add(self):
@@ -25,3 +41,15 @@ class Performance(models.Model):
 
     def __str__(self):
         return '%s %s' % (self.venue, self.date)
+
+
+class Image(models.Model):
+    work = models.ForeignKey(Work, null=True)
+    name = models.CharField(max_length=100)
+    url = models.URLField(max_length=200)
+
+    def add(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
