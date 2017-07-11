c25199c345a0ef3b19538e1482c9f7f36e3bbe1c
ericheep <ericheep@gmail.com>
2017-07-10 22:44:42 -0700

    snark

--- miscellany/settings.py
+++ miscellany/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# But then, watch me.
 DEBUG = True
 
 ALLOWED_HOSTS = [
