a7689738117468c7d28e76f5e95040053119e9f6
ericheep <ericheep@gmail.com>
2017-07-13 13:30:45 -0700

    got rid of some old files, added p5 to index

--- portfolio/templatetags/tag_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from portfolio.models import Tag
-
-register = template.Library()
-
-
-@register.simple_tag
-def get_tags(tag_slug):
-    return Tag.objects.exclude(slug=tag_slug)
