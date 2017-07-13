2e29ecb96710b5b6da9b7d12e9192f58f809552f
ericheep <ericheep@gmail.com>
2017-07-13 13:45:16 -0700

    turning on prod

--- miscellany/settings.py
+++ miscellany/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# But then, watch me.
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'www.ericheep.com',
