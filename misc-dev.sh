4563ffad0865a4f79196969208de4bca76bd5a99
ericheep <ericheep@gmail.com>
2018-07-21 12:36:16 -0700

    :wrench: change settings yet again

--- misc-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd '$HOME/git/miscellany'
-tmux new-session -d
-tmux split-window -h
-tmux split-window -v '/$HOME/misc/bin/python3 /$HOME/git/miscellany/manage.py runserver'
-tmux select-pane -t 0
-tmux -2 attach-session -d
